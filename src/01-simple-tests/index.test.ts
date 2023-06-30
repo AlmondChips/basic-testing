9994c3c65ad02628d1f4b02f4e57e18672f56651
AlmondChips <chips6t@gmail.com>
2023-06-30 15:47:25 +0300

    feat: add tests for 01 and 02 tasks

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,38 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action, RawCalculatorInput } from './index';
+
+const createTestData = (Action: Action | unknown): RawCalculatorInput => {
+  return { a: 4, b: 2, action: Action };
+};
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator(createTestData(Action.Add))).toBe(6);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator(createTestData(Action.Subtract))).toBe(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator(createTestData(Action.Multiply))).toBe(8);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator(createTestData(Action.Divide))).toBe(2);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator(createTestData(Action.Exponentiate))).toBe(16);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    expect(simpleCalculator(createTestData('Invalid action'))).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(
+      simpleCalculator(createTestData({ a: 'sd', b: 3.4, action: Action.Add })),
+    ).toBeNull();
   });
 });
